46ab659bebab16dc27d581e611e2b2bc0b961e7d
Stef van Grieken <svangrieken@dhcp-172-17-94-226.mtv.corp.google.com>
2013-04-23 12:19:41 -0700

    Test update

--- test/test.dart
+++ test/test.dart
@@ -3,12 +3,22 @@
 
 import 'dart:io';
 import 'package:web_ui/testing/render_test.dart';
-import 'package:unittest/compact_vm_config.dart';
 import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+import 'package:unittest/vm_config.dart';
+
+import '../web/model.dart';
+import '../web/view.dart';
 
 void main() {
-
+  //Set up client side HTML configuration
+  
+  useHtmlConfiguration();
+  
   group('Metrics', () {
-    //TODO: build unit tests
+    test("Metric constructor", (){
+    var item = new Metric('Minimum RTT','rtt_min','msec','RoundtripTime',8,20000,7,2012);
+    expect(item, isNotNull);
+    });
   });
 }
