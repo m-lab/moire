ee35b67e8e1e32ef09e673a4c8c7d79b43ac1a8b
Dominic Hamon <dominic@measurementlab.net>
2013-05-06 14:35:23 -0700

    metric_div.dart separate

--- web/metric_div.dart
+++ web/metric_div.dart
@@ -1,1 +1,21 @@
+import 'package:web_ui/web_ui.dart';
+import 'moire.dart';
 
+class MetricDiv extends WebComponent {
+  View view;
+  String type;
+  
+  String get name => view.showMetricName(type);
+  String get description => view.showMetricDefinition(type);
+  
+  @observable
+  String average;
+  
+  @observable
+  String change;
+  
+  void inserted() {
+    view.showMetric(type).then((String s) => average = s);
+    view.showMetricChange(type).then((String s) => change = s);
+  }
+}
