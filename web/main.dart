fba88ac2cf6164b5f48188c98d9053407b419f60
Stef van Grieken <stef@van-grieken.nl>
2013-05-06 13:49:41 -0700

    added component

--- web/main.dart
+++ web/main.dart
@@ -20,3 +20,4 @@ main() {
 
   String avgDownloadSpeed = '258';
 }
+
--- web/moire.dart
+++ web/moire.dart
@@ -6,7 +6,6 @@ import 'dart:html';
 import 'package:js/js.dart' as js;
 import 'package:web_ui/watcher.dart' as watchers;
 import 'ranking.dart';
-
 part 'view.dart';
 part 'controller.dart';
 part 'metric.dart';
@@ -17,4 +16,5 @@ final View view = new View();
 final Controller controller = new Controller();
 
 const String kMetricsAPIUrl = "http://mlab-metrics-api-server.appspot.com/api";
+
 
